7fd048530ce1c5294105da18ac964dc2a328f1e9
Francisco Javier <fbottazzini@uade.edu.ar>
2024-10-21 23:55:32 -0300

    Version Entrega

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import AddProduct from './components/productos/AddProduct';
-import Register from './components/Register';
-import FormLogin from './components/FormLogin';
-import './App.css'; // Importa los estilos globales de la aplicación
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList'; 
+import FormLogin from './components/FormLogin'; 
+import Cart from './components/Cart'; 
+import Navbar from './components/NavBar'; 
+import AddProducts from './components/productos/AddProduct'
+import Register from './components/Register'
+import { CartManager } from './components/CartManager'; 
+import './App.css'; 
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
   return (
-    
-      <div className="app">
-        <FormLogin /> 
-        <Register />
-        <AddProduct />
-        
-    {/* Renderiza la barra de navegación */}
-       
-      </div>
-   
+    <CartManager>
+      {/* El Router ya está en main.jsx, aquí solo definimos las rutas */}
+      <Navbar toggleCart={toggleCart} />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/productos" element={<ProductList />} />
+        <Route path="/agregar" element={<AddProducts />} />
+        <Route path="/login" element={<FormLogin />} />
+        <Route path="/registrarse" element={<Register />} />
+      </Routes>
+      <Cart isOpen={isCartOpen} toggleCart={toggleCart} />
+    </CartManager>
   );
 }
-
 
 export default App;
